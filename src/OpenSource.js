57a150d159926f4b9191f74a885970f716fa40c2
MarinaAreias <marinasareias@gmail.com>
2020-11-22 17:16:23 +0100

    added footer with open-source

---
+++ src/OpenSource.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./OpenSource.css"; 
+
+
+export default function Ola(){
+    return ( 
+
+    <footer className="open-source">
+<a href= "https://github.com/MarinaAreias/weather-app-react"
+    target= "_black"
+    rel="noreferrer">
+    Open-source code
+</a>
+,  by Marina Areias
+
+    </footer>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import OpenSource from './OpenSource';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  
+    <OpenSource/>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
